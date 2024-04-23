f1125737461371791600036fb2d1f9e1d5b0b5a1
hudamiftahul <mfthul.hd@gmail.com>
2024-04-24 00:19:16 +0700

    perbaruan limit access page

--- application/views/access_limit.php
+++ application/views/access_limit.php
@@ -1,37 +1,30 @@
-<!DOCTYPE html>
-<html>
-
+<html lang="en">
 <head>
-    <title>Access Denied</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <style>
-        body {
-            background-color: black;
-            color: white;
-        }
-
-        h1 {
-            color: red;
-        }
-
-        h6 {
-            color: red;
-            text-decoration: underline;
-        }
-    </style>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<link href='https://fonts.googleapis.com/css?family=Lato:300,400|Montserrat:700' rel='stylesheet' type='text/css'>
+	<style>
+		@import url(//cdnjs.cloudflare.com/ajax/libs/normalize/3.0.1/normalize.min.css);
+		@import url(//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css);
+	</style>
+	<link rel="stylesheet" href="https://2-22-4-dot-lead-pages.appspot.com/static/lp918/min/default_thank_you.css">
+	<script src="https://2-22-4-dot-lead-pages.appspot.com/static/lp918/min/jquery-1.9.1.min.js"></script>
+	<script src="https://2-22-4-dot-lead-pages.appspot.com/static/lp918/min/html5shiv.js"></script>
 </head>
-
 <body>
-    <div class="w3-display-middle">
-        <h1 class="w3-jumbo w3-animate-top w3-center"><code>Terimakasih telah melakukan pengisian!!</code></h1>
-        <hr class="w3-border-white w3-animate-left" style="margin:auto;width:50%">
-            <h3 class="w3-center w3-animate-right">Anda telah melebihi batas maksimal pengisian.</h3>
-            <h3 class="w3-center w3-animate-zoom">🚫🚫🚫🚫</h3>
-            <h6 class="w3-center w3-animate-zoom"><a href="<?= base_url('dashboard') ?>">Kembali ke Dashboard</a></h6>
-    </div>
-</body>
+	<header class="site-header" id="header">
+		<h1 class="site-header__title" data-lead-id="site-header-title">TERIMAKASIH!</h1>
+	</header>
 
+	<div class="main-content">
+		<i class="fa fa-check main-content__checkmark" id="checkmark"></i>
+		<p class="main-content__body" data-lead-id="main-content-body">Anda telah mencapai batas maksimal inputan. Maks 10 Inputan setiap tahunnya.</p>
+	</div>
+
+	<footer class="site-footer" id="footer" style="margin-top: -80px">
+		<p class="site-footer__fineprint" id="fineprint">Copyright ©2024 | Bagian Organisasi Kota Pasuruan</p>
+	</footer>
+</body>
 </html>
